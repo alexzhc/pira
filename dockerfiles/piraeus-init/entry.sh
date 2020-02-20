26971ee32b0be6a0ca53127ba6ce438d13e2ede1
alexzhc <alexzhc@hotmail.com>
2020-02-21 06:37:14 +0800

    set controller to use hostnetwork and bind HOST_IP:3370; set node proxy to bind 127.0.0.1:3370

--- dockerfiles/piraeus-init/entry.sh
+++ dockerfiles/piraeus-init/entry.sh
@@ -5,13 +5,17 @@ ${INIT_DEBUG,,} && set -x
 cp -r /files/* /init
 
 # configure each component
+echo "* This pod name is ${THIS_POD_NAME}"
 if [[ "${THIS_POD_NAME}" =~ -etcd-[0-9]+$ ]]; then
-    /init/bin/config-etcd.sh
-elif [[ "${THIS_POD_NAME}" =~ -controller-[0-9a-z]{10}-[0-9a-z]{5}$ ]]; then
-    /init/bin/config-controller.sh
+    echo "* Initialize etcd"
+    /init/bin/init-etcd.sh
+elif [[ "${THIS_POD_NAME}" =~ -controller-[0-9a-z]+-[0-9a-z]+$ ]]; then
+    echo "* Initialize controller"
+    /init/bin/init-controller.sh
 elif [[ "${THIS_POD_NAME}" =~ -node-[0-9a-z]+$ ]]; then
-    /init/bin/config-node.sh
+    echo "* Initialize node"
+    /init/bin/init-node.sh
 else
-    echo "Failed to identify the component"
+    echo "* Failed to identify the component"
     exit 1
 fi
